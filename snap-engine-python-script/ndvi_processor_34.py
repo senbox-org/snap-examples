11e18919cb08d38a6d218a637dc285993c42523a
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2015-08-21 11:36:29 +0200

    Merged master

--- snap-engine-python-script/ndvi_processor_34.py
+++ snap-engine-python-script/ndvi_processor_34.py
@@ -6,34 +6,48 @@ import sys
 sys.path.append('C:\\Users\\Norman\\JavaProjects\\senbox')
 
 import snappy
+#print('snappy.__file__:', snappy.__file__)
+
+from snappy import jpy
+from snappy import GPF
+from snappy import ProductIO
 
 ProductIOPlugInManager = snappy.jpy.get_type('org.esa.snap.framework.dataio.ProductIOPlugInManager')
-Logger = snappy.jpy.get_type('java.util.logging.Logger')
-Level = snappy.jpy.get_type('java.util.logging.Level')
-HashMap = snappy.jpy.get_type('java.util.HashMap')
+Logger = jpy.get_type('java.util.logging.Logger')
+Level = jpy.get_type('java.util.logging.Level')
+Arrays = jpy.get_type('java.util.Arrays')
+File = jpy.get_type('java.io.File')
+String = jpy.get_type('java.lang.String')
+HashMap = jpy.get_type('java.util.HashMap')
+
 Logger.getLogger('').setLevel(Level.OFF)
 snappy.SystemUtils.LOG.setLevel(Level.OFF)
 
-snappy.SystemUtils.init3rdPartyLibs(None)
+
 
 reader_spi_it = ProductIOPlugInManager.getInstance().getAllReaderPlugIns()
 while reader_spi_it.hasNext():
     reader_spi = reader_spi_it.next()
-    print("reader_spi:", reader_spi.getClass())
-
-snappy.GPF.getDefaultInstance().getOperatorSpiRegistry().loadOperatorSpis()
+    print("reader_spi: " + ', '.join(reader_spi.getFormatNames()) + " (" + reader_spi.getClass().getName() + ")")
 
 
-op_spi_it = snappy.GPF.getDefaultInstance().getOperatorSpiRegistry().getOperatorSpis().iterator()
+writer_spi_it = ProductIOPlugInManager.getInstance().getAllWriterPlugIns()
+while writer_spi_it.hasNext():
+    writer_spi = writer_spi_it.next()
+    print("writer_spi: " + ', '.join(writer_spi.getFormatNames()) + " (" + writer_spi.getClass().getName() + ")")
+
+
+GPF.getDefaultInstance().getOperatorSpiRegistry().loadOperatorSpis()
+op_spi_it = GPF.getDefaultInstance().getOperatorSpiRegistry().getOperatorSpis().iterator()
 while op_spi_it.hasNext():
     op_spi = op_spi_it.next()
     print("op_spi:", op_spi.getOperatorAlias())
 
 
 parameters = HashMap()
-parameters.put('file', snappy.File('L3_subset_watermask.dim'))
-p = snappy.GPF.createProduct("Read", parameters)
-#p = snappy.ProductIO.readProduct('L3_subset_watermask.dim')
+parameters.put('file', File('L3_subset_watermask.dim'))
+p = GPF.createProduct("Read", parameters)
+#p = ProductIO.readProduct('L3_subset_watermask.dim')
 
 name = p.getName()
 w = p.getSceneRasterWidth()
