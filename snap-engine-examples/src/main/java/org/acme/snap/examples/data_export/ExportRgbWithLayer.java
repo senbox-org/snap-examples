7944ac2aaa2a0b8d4734745df4e1c754df0613d8
marpet <marco.peters@brockmann-consult.de>
2016-10-11 17:03:43 +0200

    updated example

--- snap-engine-examples/src/main/java/org/acme/snap/examples/data_export/ExportRgbWithLayer.java
+++ snap-engine-examples/src/main/java/org/acme/snap/examples/data_export/ExportRgbWithLayer.java
@@ -5,7 +5,10 @@ import com.bc.ceres.glayer.CollectionLayer;
 import com.bc.ceres.glayer.Layer;
 import com.bc.ceres.glayer.LayerContext;
 import com.bc.ceres.glayer.support.ImageLayer;
+import com.bc.ceres.glevel.MultiLevelModel;
+import com.bc.ceres.grender.Viewport;
 import com.bc.ceres.grender.support.BufferedImageRendering;
+import com.bc.ceres.grender.support.DefaultViewport;
 import org.esa.snap.core.dataio.ProductIO;
 import org.esa.snap.core.datamodel.Band;
 import org.esa.snap.core.datamodel.ImageInfo;
@@ -19,6 +22,9 @@ import org.esa.snap.core.util.io.FileUtils;
 import org.esa.snap.ui.product.VectorDataLayer;
 
 import javax.media.jai.JAI;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 import java.awt.image.RenderedImage;
 import java.io.File;
@@ -35,7 +41,7 @@ public class ExportRgbWithLayer {
         File vectorFile = new File(args[1]);
         File rgbFile = new File(args[2]);
 
-        String[] rgBbandNames = new String[]{"radiance_7", "radiance_5", "radiance_2", };
+        String[] rgBbandNames = new String[]{"radiance_1", "radiance_1", "radiance_1", };
         HashMap<String, Object> parameters = new HashMap<>();
         parameters.put("vectorFile", vectorFile);
         parameters.put("separateShapes", false);
@@ -61,26 +67,46 @@ public class ExportRgbWithLayer {
         CollectionLayer collectionLayer = new CollectionLayer();
         LayerContext ctx = new MyLayerContext(product, collectionLayer);
         SceneTransformProvider provider = bands[0];
+        MultiLevelModel multiLevelModel = bands[0].getMultiLevelModel();
 
         // RGB
-        ImageLayer RGBLayer = new ImageLayer(renderedRGB);
+        ImageLayer RGBLayer = new ImageLayer(renderedRGB, bands[0].getImageToModelTransform(), 1);
 
         // Vector
         VectorDataNode vectorDataNode = product.getVectorDataGroup().get(vectorName);
         VectorDataLayer vectorDataLayer = new VectorDataLayer(ctx, vectorDataNode, provider );
 
-        BufferedImage buffered = new BufferedImage(renderedRGB.getWidth(), renderedRGB.getHeight(), BufferedImage.TYPE_INT_ARGB);
-        BufferedImageRendering rendering = new BufferedImageRendering(buffered);
-
     /* Create the complete image by overlaying */
         collectionLayer.getChildren().add(vectorDataLayer);
         collectionLayer.getChildren().add(RGBLayer);
 
+        BufferedImage buffered = new BufferedImage(renderedRGB.getWidth(), renderedRGB.getHeight(), BufferedImage.TYPE_INT_ARGB);
+        BufferedImageRendering rendering = createRendering(buffered, multiLevelModel);
         collectionLayer.render(rendering);
 
         JAI.create("filestore", rendering.getImage(), rgbFile.toString(), "PNG");
 
     }
+
+    private static BufferedImageRendering createRendering(BufferedImage bufferedImage, MultiLevelModel multiLevelModel) {
+        AffineTransform m2iTransform = multiLevelModel.getModelToImageTransform(0);
+        final Viewport vp2 = new DefaultViewport(new Rectangle(bufferedImage.getWidth(), bufferedImage.getHeight()),
+                                                 m2iTransform.getDeterminant() > 0.0);
+        vp2.zoom(multiLevelModel.getModelBounds());
+
+        final BufferedImageRendering imageRendering = new BufferedImageRendering(bufferedImage, vp2);
+        // because image to model transform is stored with the exported image we have to invert
+        // image to view transformation
+        final AffineTransform v2mTransform = vp2.getViewToModelTransform();
+        v2mTransform.preConcatenate(m2iTransform);
+        final AffineTransform v2iTransform = new AffineTransform(v2mTransform);
+
+        final Graphics2D graphics2D = imageRendering.getGraphics();
+        v2iTransform.concatenate(graphics2D.getTransform());
+        graphics2D.setTransform(v2iTransform);
+        return imageRendering;
+    }
+
 
     private static class MyLayerContext implements LayerContext {
 
